e2160fad0ed9e3ab85a35318de89d9e596aad61e
TjgL <lithmoneo@gmail.com>
2025-03-11 15:40:09 +0100

    Implemented rotating lever

---
+++ Source/BallPlatorm/Private/Mechanics/PRotatingLever.cpp
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mechanics/PRotatingLever.h"
+
+#include "Kismet/KismetMathLibrary.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+
+
+APRotatingLever::APRotatingLever()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	const FName CenterMeshName = FName(TEXT("Center Mesh"));
+	CenterMesh = CreateDefaultSubobject<UStaticMeshComponent>(CenterMeshName);
+	CenterMesh->SetupAttachment(RootComponent);
+
+	const FName RotatingMeshName = FName(TEXT("Rotating Mesh"));
+	RotatingMesh = CreateDefaultSubobject<UStaticMeshComponent>(RotatingMeshName);
+	RotatingMesh->SetupAttachment(RootComponent);
+
+	Constraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(TEXT("Constraint"));
+	Constraint->SetupAttachment(RootComponent);
+	Constraint->ComponentName1 = FConstrainComponentPropName(CenterMeshName);
+	Constraint->ComponentName2 = FConstrainComponentPropName(RotatingMeshName);
+}
+
+void APRotatingLever::BeginPlay()
+{
+	Super::BeginPlay();
+	PreviousRotation = RotatingMesh->GetComponentRotation();
+}
+
+void APRotatingLever::Tick(const float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	CheckRotationProgress();
+}
+
+void APRotatingLever::CheckRotationProgress()
+{
+	const FRotator CurrentRotation = RotatingMesh->GetComponentRotation();
+	const float RotationDelta = UKismetMathLibrary::NormalizedDeltaRotator(CurrentRotation, PreviousRotation).Yaw;
+
+	RotationDone += RotationDelta;
+	PreviousRotation = CurrentRotation;
+
+	if (RotationDone <= 0 || RotationDone >= Limits)
+	{
+		Constraint->SetAngularSwing1Limit(ACM_Limited, 0);
+	}
+	else
+	{
+		Constraint->SetAngularSwing1Limit(ACM_Free, 0);
+	}
+}
+
---
+++ Source/BallPlatorm/Public/Mechanics/PRotatingLever.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PRotatingLever.generated.h"
+
+class UPhysicsConstraintComponent;
+
+UCLASS()
+class BALLPLATORM_API APRotatingLever : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APRotatingLever();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	float GetRotationPercentage() const { return RotationDone / Limits; }
+
+private:
+	void CheckRotationProgress();
+
+private: // Settings
+	UPROPERTY(EditAnywhere, Category = Platformer, meta = (AllowPrivateAccess = true))
+	float RotationDone = 0.f;
+
+	UPROPERTY(EditAnywhere, Category = Platformer, meta = (AllowPrivateAccess = true))
+	FRotator PreviousRotation = FRotator::ZeroRotator;
+
+	UPROPERTY(EditAnywhere, Category = Platformer, meta = (AllowPrivateAccess = true, UIMin = 0))
+	float Limits = 360.f;
+
+private: // Components
+	UPROPERTY(EditAnywhere, Category = Platformer, meta = (AllowPrivateAccess = true))
+	TObjectPtr<UMeshComponent> CenterMesh;
+
+	UPROPERTY(EditAnywhere, Category = Platformer, meta = (AllowPrivateAccess = true))
+	TObjectPtr<UMeshComponent> RotatingMesh;
+
+	UPROPERTY(EditAnywhere, Category = Platformer, meta = (AllowPrivateAccess = true))
+	TObjectPtr<UPhysicsConstraintComponent> Constraint;
+};
